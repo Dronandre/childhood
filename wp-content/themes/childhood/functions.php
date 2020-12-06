8b87ba0168bfe4a38ea428bd6212fb2b208fbf98
Dronandre <dronandre@yandex.ru>
2020-12-06 17:24:52 +0700

    Add customs fields

--- wp-content/themes/childhood/functions.php
+++ wp-content/themes/childhood/functions.php
@@ -6,3 +6,5 @@
         wp_enqueue_style( 'childhood-style', get_stylesheet_uri() );
         wp_enqueue_script( 'childhood_scripts', get_template_directory_uri( ) . '/assets/js/main.min.js', array(), null, true);
     };
+
+    add_theme_support('custom-logo');
